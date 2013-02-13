b4249c974921f6c711fed5dd110741d3a58ede9c
Andrew Danner <adanner@cs.swarthmore.edu>
2013-02-13 00:08:36 -0500

    get rid of TODO about QGLFormat to avoid confusion

--- projects/02/main.cpp
+++ projects/02/main.cpp
@@ -1,11 +1,8 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QGLFormat>
 
 int main(int argc, char *argv[])
 {
-    /*TODO: Set format before initializing application*/
-
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
