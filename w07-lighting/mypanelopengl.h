ce24a49fd427cf178a60fab1d1ad0e56f3434fab
Andrew Danner <adanner@cs.swarthmore.edu>
2013-03-04 08:09:54 -0500

    starting lighting demo

---
+++ w07-lighting/mypanelopengl.h
@@ -0,0 +1,75 @@
+#ifndef MYPANELOPENGL_H
+#define MYPANELOPENGL_H
+
+#include <QGLWidget>
+#include <QtOpenGL>
+#include <QMatrix4x4>
+#include "common/sphere.h"
+#include "common/square.h"
+
+typedef QVector4D point4;
+typedef QVector4D color4;
+typedef QVector3D vec3;
+typedef QMatrix4x4 mat4;
+
+class MyPanelOpenGL : public QGLWidget
+{
+    Q_OBJECT
+
+protected:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+    void keyPressEvent(QKeyEvent* event);
+
+public:
+    explicit MyPanelOpenGL(QWidget *parent = 0);
+    virtual ~MyPanelOpenGL();
+
+private:
+
+    /* simple test shapes */
+    cs40::Sphere* m_sphere;
+    cs40::Square* m_square;
+
+    int m_polymode;
+    bool m_drawSphere;
+
+    GLuint m_textureID;
+
+    vec3 m_angles; /* Euler angles for rotation */
+
+    mat4 m_model;
+    mat4 m_camera;
+    mat4 m_projection;
+
+    /* Shaders and program */
+    QGLShader *m_vertexShader;
+    QGLShader *m_fragmentShader;
+    QGLShaderProgram *m_shaderProgram;
+
+    /* update Euler angle at index idx by amt
+     * idx=0,1,2 -> x,y,z */
+    void updateAngles(int idx, qreal amt);
+
+    /* wrap a angle value to the range 0..360*/
+    qreal wrap(qreal amt);
+
+    /* update Polygon draw mode based
+     * 0 : point
+     * 1 : line
+     * 2 : polygon */
+    void updatePolyMode(int val);
+
+    void createVBO();
+    void destroyVBO();
+    void createShaders();
+    void destroyShaders();
+
+signals:
+    
+public slots:
+
+};
+
+#endif // MYPANELOPENGL_H
---
+++ w07-lighting/vshader.glsl
@@ -0,0 +1,43 @@
+#version 130
+
+uniform vec4 vColor;
+uniform vec3 theta;
+
+uniform mat4 model;
+uniform mat4 camera;
+uniform mat4 projection;
+
+in vec4 vPosition;
+in vec2 vTexture;
+in vec3 vNormal;
+
+out vec4 color;
+
+void main() 
+{
+    // Compute the sines and cosines of theta for each of
+    //   the three axes in one computation.
+    vec3 angles = radians( theta );
+    vec3 c = cos( angles );
+    vec3 s = sin( angles );
+
+    // GLSL matrices are column-major
+    mat4 rx = mat4( 1.0,  0.0,  0.0, 0.0,
+                    0.0,  c.x,  s.x, 0.0,
+                    0.0, -s.x,  c.x, 0.0,
+                    0.0,  0.0,  0.0, 1.0 );
+
+    mat4 ry = mat4( c.y, 0.0, -s.y, 0.0,
+                    0.0, 1.0,  0.0, 0.0,
+                    s.y, 0.0,  c.y, 0.0,
+                    0.0, 0.0,  0.0, 1.0 );
+
+    mat4 rz = mat4( c.z,  s.z, 0.0, 0.0,
+                   -s.z,  c.z, 0.0, 0.0,
+                    0.0,  0.0, 1.0, 0.0,
+                    0.0,  0.0, 0.0, 1.0 );
+
+    gl_Position = projection*camera*rz*ry*rx*model*vPosition;
+    color = vColor;
+
+} 
