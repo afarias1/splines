5820eea083b1a1c2707e8fc82d891070336529b7
Andrew Danner <adanner@cs.swarthmore.edu>
2013-03-26 19:48:08 -0400

    w09-grammar start

---
+++ w09-grammar/mypanelopengl.h
@@ -0,0 +1,86 @@
+#ifndef MYPANELOPENGL_H
+#define MYPANELOPENGL_H
+
+//<QtOpenGL> causes some headaches.
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glext.h>
+#include <QKeyEvent>
+#include <QGLWidget>
+#include <QMatrix4x4>
+#include "common/sphere.h"
+#include "common/square.h"
+
+typedef QVector4D point4;
+typedef QVector4D color4;
+typedef QVector3D vec3;
+typedef QMatrix4x4 mat4;
+
+#define CS40_NUM_PROGS 1
+
+class MyPanelOpenGL : public QGLWidget
+{
+    Q_OBJECT
+
+protected:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+    void keyPressEvent(QKeyEvent* event);
+
+public:
+    explicit MyPanelOpenGL(QWidget *parent = 0);
+    virtual ~MyPanelOpenGL();
+
+private:
+
+    /* simple test shapes */
+    cs40::Sphere* m_sphere;
+    cs40::Square* m_square;
+
+    int m_polymode;
+    int m_normal_map;
+    bool m_drawSphere;
+
+    GLuint m_textureID;
+    GLuint m_normalMapID;
+
+    vec3 m_angles; /* Euler angles for rotation */
+
+    mat4 m_model;
+    mat4 m_camera;
+    mat4 m_projection;
+
+    /* Shaders and programs */
+    QGLShader *m_vertexShaders[CS40_NUM_PROGS];
+    QGLShader *m_fragmentShaders[CS40_NUM_PROGS];
+    QGLShaderProgram *m_shaderPrograms[CS40_NUM_PROGS];
+    int m_curr_prog; //current program ID
+
+
+    /* update Euler angle at index idx by amt
+     * idx=0,1,2 -> x,y,z */
+    void updateAngles(int idx, qreal amt);
+
+    /* update model matrix based on angle */
+    void updateModel();
+
+    /* wrap a angle value to the range 0..360*/
+    qreal wrap(qreal amt);
+
+    /* update Polygon draw mode based
+     * 0 : point
+     * 1 : line
+     * 2 : polygon */
+    void updatePolyMode(int val);
+
+    void createShaders(int i, QString vertName, QString fragName);
+    void destroyShaders(int i);
+
+signals:
+    
+public slots:
+
+};
+
+#endif // MYPANELOPENGL_H
---
+++ w09-grammar/test_noise.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+
+#include "common/rgbImage.h"
+#include "common/noise.h"
+
+using namespace std;
+using namespace cs40;
+
+NoiseGenerator ngen;
+
+/* convert from -1-1 noise space to 0-255 greyscale */
+RGBColor convertColor(float x){
+  int r,g,b;
+  x=0.5*(1+x); //map to 0..1
+  r=(int)(255*x);
+  g=(int)(255*x);
+  b=(int)(255*x);
+  return RGBColor(r,g,b);
+}
+
+float surf1(float x, float y){
+   float freq=0.05;
+   float amp = 0.5;
+   float steps = 2;
+   float val = 0;
+   while(steps > 0){
+     val += amp*ngen.noise2(x,y,freq);
+     amp*=0.5;
+     freq*=2;
+     steps--;
+   }
+   return val;
+}
+
+float surf2(float x, float y){
+   float freq=0.05;
+   float amp = 0.5;
+   float steps = 2;
+   float val = 0;
+   while(steps > 0){
+     val += fabs(amp*ngen.noise2(x,y,freq));
+     amp*=0.5;
+     freq*=2;
+     steps--;
+   }
+   return val;
+}
+
+float surf3(float x, float y){
+
+   return sin(3*M_PI*(x+surf1(x,y)));
+}
+
+float surf4(float x, float y){
+  float val = 5*ngen.noise2(x,y,0.01);
+  return val-static_cast<int>(val);
+}
+
+float surf5(float x, float y, float z){
+   float freq=0.05;
+   float amp = 0.5;
+   float steps = 2;
+   float val = 0;
+   while(steps > 0){
+     val += fabs(amp*ngen.noise3(x,y,z,freq));
+     amp*=0.5;
+     freq*=2;
+     steps--;
+   }
+   return sin(3*M_PI*(x+val));
+}
+int main(int argc, char* argv[]){
+  srand(time(NULL));
+  string fname = "test.png";
+  int nrows=256, ncols=256;
+  RGBImage img(nrows, ncols, RGBColor(255,255,255));
+
+  float x,y,val;
+  for(int j=0; j<nrows; j++){
+    for(int i=0; i<ncols; i++){
+      x = (i+0.5)/ncols-0.5;
+      y = (j+0.5)/nrows-0.5;
+      val = surf2(x,y);
+      img(j,i) = convertColor(val);
+    }
+  }
+
+  img.saveAs(fname, true);
+
+  cout << "Saved result to " << fname << endl;
+  return 0;
+}
+
